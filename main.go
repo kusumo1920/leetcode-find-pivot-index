2c2dd271c955d214b25c9b2232178526b4f62a4a
Kesit Budi Kusumo <kusumo1920@kesit-almalinux-pc.kernel.id>
2024-06-14 22:41:38 +0700

    feat: add iterative solution

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	input := []int{2, 1, -1}
+	output := pivotIndex(input)
+	fmt.Println(output)
+}
+
+func pivotIndex(nums []int) int {
+	sum, leftSum := 0, 0
+	for _, v := range nums {
+		sum += v
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if leftSum == sum-nums[i]-leftSum {
+			return i
+		}
+
+		leftSum += nums[i]
+	}
+
+	return -1
 }
